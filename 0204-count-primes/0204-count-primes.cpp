a8d542e868f5273560647fa06ab4f42e0080ae64
jasneet-arora27 <120542196+jasneet-arora27@users.noreply.github.com>
2023-06-13 14:05:18 +0530

    Time: 309 ms (35.06%), Space: 130.5 MB (11.26%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        int* prime = new int[n];
+        for (int i = 2; i < n; i++) {
+            prime[i] = 0;
+        }
+        int count = 0;
+
+        for (long long i = 2; i < n; i++) {
+            if (prime[i] == 0) {
+                for (long long j = i * i; j < n; j += i) {
+                    prime[j] = 1;
+                }
+                count++;
+            }
+        }
+
+        delete[] prime;
+
+        return count;
+    }
+};
