ca3e81debcdcb49b5e434b1b8ef716dfd910463c
jasneet-arora27 <120542196+jasneet-arora27@users.noreply.github.com>
2023-06-27 13:05:10 +0530

    Time: 3 ms (47.89%), Space: 8.6 MB (98.55%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) 
+    {
+        int n = digits.size();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+        
+        // If we reach here, it means all digits were 9
+        digits.insert(digits.begin(), 1);
+        
+        return digits;
+    }
+};
