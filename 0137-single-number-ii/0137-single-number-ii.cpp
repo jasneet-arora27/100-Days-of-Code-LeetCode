38729fa420a6932486aa3260b020ef75995dd481
Jasneet Arora <120542196+jasneet-arora27@users.noreply.github.com>
2023-08-10 13:05:32 +0530

    Time: 9 ms (57.52%), Space: 9.6 MB (40.87%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int n = nums.size();
+
+        if (n == 1) 
+        {
+            return nums[0];
+        }
+
+        sort(nums.begin(), nums.end());
+
+        if (nums[0] != nums[1]) 
+        {
+            return nums[0];
+        }
+
+        if (nums[n - 1] != nums[n - 2]) 
+        {
+            return nums[n - 1];
+        }
+
+        for (int i = 1; i < n - 1; i++) 
+        {
+            if ((nums[i] != nums[i - 1]) && (nums[i] != nums[i + 1])) 
+            {
+                return nums[i];
+            }
+        }
+
+        // else
+        return -1;
+    }
+};
